9993c0515f1e664bf4eb5a57c656d966a97cb91f
Susie <susie.kim@hotmail.com>
2019-02-05 19:38:47 -0500

    susie's first move

--- src/yourMoves.js
+++ src/yourMoves.js
@@ -1,6 +1,6 @@
 export const x = "❌";
 export const o = "⭕️";
 
-const moves = [[[], [], []], [[], [], []], [[], [], []]];
+const moves = [[[], [o], []], [[], [], []], [[], [], []]];
 
 export default moves;
